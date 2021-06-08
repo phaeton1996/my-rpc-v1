d0a115a6fb73389759780f9f01d622699a97abd0
phaeton <542068262@qq.com>
2021-06-08 18:02:35 +0800

    first commit

---
+++ rpc-common/src/main/java/com/rpc/common/util/StringUtil.java
@@ -0,0 +1,32 @@
+package com.rpc.common.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class StringUtil {
+
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isEmpty(String str) {
+        if (str != null) {
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+    }
+
+    /**
+     * 判断字符串是否非空
+     */
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
+    }
+
+    /**
+     * 分割固定格式的字符串
+     * StringUtils.splitByWholeSeparator(":ab::cd:ef", ":")
+     *  -> ["ab", "cd", "ef"]
+     */
+    public static String[] split(String str, String separator) {
+        return StringUtils.splitByWholeSeparator(str, separator);
+    }
+}
